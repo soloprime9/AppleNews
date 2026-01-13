14f8722476c3e737f5c9b3b04fb9a5bf001b8acd
soloprime9 <primeindia5750@gmail.com>
2026-01-13 08:15:50 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,18 +1,46 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGODB_URL = process.env.MONGODB_URL;
 
-console.log("Mongo URL:", MONGO_URI);
+console.log("Mongo URL:", MONGODB_URL);
 
-if (!MONGO_URI) {
+if (!MONGODB_URL) {
   console.error("❌ MONGO_URI not found in environment variables");
   process.exit(1);
 }
 
-mongoose.connect(MONGO_URI)
+mongoose.connect(MONGODB_URL)
   .then(() => {
     console.log("✅ MongoDB Connected Successfully");
   })
   .catch((err) => {
     console.error("❌ MongoDB Connection Failed:", err.message);
   });
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require("mongoose");
+
+// const MONGO_URI = process.env.MONGO_URI;
+
+// console.log("Mongo URL:", MONGO_URI);
+
+// if (!MONGO_URI) {
+//   console.error("❌ MONGO_URI not found in environment variables");
+//   process.exit(1);
+// }
+
+// mongoose.connect(MONGO_URI)
+//   .then(() => {
+//     console.log("✅ MongoDB Connected Successfully");
+//   })
+//   .catch((err) => {
+//     console.error("❌ MongoDB Connection Failed:", err.message);
+//   });
