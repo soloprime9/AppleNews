1fd22929edeaf1601dae56695cae863432efecef
soloprime9 <primeindia5750@gmail.com>
2026-01-13 09:26:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ['https://computer-xrfg.vercel.app']
+  origin: ['https://computer-xrfg.vercel.app','https://news.fondpeace.com']
 }));
 
 app.get("/", async (req, res) => {
