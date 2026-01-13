e8cf7385990f41a5329540089b10c12a9ca0cea9
soloprime9 <primeindia5750@gmail.com>
2026-01-13 21:53:13 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,3 +1,28 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 10000,
+    });
+
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection failed:", err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+
+
+
+
+
+
+
+
+
 // const mongoose = require("mongoose");
 
 // const MONGODB_URL = process.env.MONGODB_URL;
@@ -25,22 +50,3 @@
 
 
 
-
-// const mongoose = require("mongoose");
-
-// const MONGO_URI = process.env.MONGO_URI;
-
-// console.log("Mongo URL:", MONGO_URI);
-
-// if (!MONGO_URI) {
-//   console.error("❌ MONGO_URI not found in environment variables");
-//   process.exit(1);
-// }
-
-// mongoose.connect(MONGO_URI)
-//   .then(() => {
-//     console.log("✅ MongoDB Connected Successfully");
-//   })
-//   .catch((err) => {
-//     console.error("❌ MongoDB Connection Failed:", err.message);
-//   });
